0784ff01a33b1651f3a4c34932f886837bc857ee
Vaibhav Lakhera <vaibhavlakhera1729@gmail.com>
2026-06-26 22:03:31 +0530

    Removed Carla1, C-shenron

--- C-Shenron/parallelization/create_jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-directory="jobs"
-
-for file in "$directory"/*.yaml; do
-    filename=$(basename "$file")
-    echo "Running kubectl create -f $directory/$filename"
-    kubectl create -f "$directory/$filename"
-done
